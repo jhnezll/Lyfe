743f50314ad26fd16499772b50940434f5366cdf
john <jhezell@go.olemiss.edu>
2021-06-15 03:09:07 -0500

    Initial Commit

---
+++ Lyfe/LyfeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyfeApp.swift
+//  Lyfe
+//
+//  Created by John on 6/15/21.
+//
+
+import SwiftUI
+
+@main
+struct LyfeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
